63b9225aa4c940fe3882e1dd2c365f560ee9e043
Nathaniel Cherian <ngcherian2014@gmail.com>
2020-04-26 23:21:32 -0700

    home tempate

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, session, request, redirect
 from config import Config
 
 
@@ -6,11 +6,18 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == "POST":
+        session['data'] = request.form['code'].strip(' ')
+        return redirect(url_for('lobby'))
+
     return render_template('home.html')
 
-
+@app.route('/lobby')
+def lobby():
+    data = session['data']
+    return  'you entered ' + data
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
